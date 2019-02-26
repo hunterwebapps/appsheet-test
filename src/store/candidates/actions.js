347d44f475415c1184195af69bc68566e889ca51
Hunter <dwaynewhunter@gmail.com>
2019-02-26 14:48:25 -0800

    change to loop

--- src/store/candidates/actions.js
+++ src/store/candidates/actions.js
@@ -1,28 +1,29 @@
 import api from '../api';
 import types from './types';
 
-export const getCandidates = (token = '') => async dispatch => {
+export const getCandidates = () => async dispatch => {
   dispatch({
     type: types.GET_CANDIDATES_REQUEST,
   });
 
-  const listResponse = await api.get(`/list?token=${token}`);
-  const candidateIds = listResponse.data.result;
-  const nextToken = listResponse.data.token;
+  let token = '';
+  do {
+    const listResponse = await api.get(`/list?token=${token}`);
+    const candidateIds = listResponse.data.result;
+    token = listResponse.data.token;
 
-  for (const candidateId of candidateIds) {
-    const detailsResponse = await api.get(`/detail/${candidateId}`);
-    dispatch({
-      type: types.ADD_CANDIDATE,
-      payload: detailsResponse.data,
-    });
-  }
+    for (const candidateId of candidateIds) {
+      const detailsResponse = await api.get(`/detail/${candidateId}`).catch(err => err);
+      if (detailsResponse && detailsResponse.data) {
+        dispatch({
+          type: types.ADD_CANDIDATE,
+          payload: detailsResponse.data,
+        });
+      }
+    }
+  } while (token)
 
-  if (token) {
-    dispatch(getCandidates(nextToken));
-  } else {
-    dispatch({
-      type: types.GET_CANDIDATES_SUCCESS,
-    });
-  }
+  dispatch({
+    type: types.GET_CANDIDATES_SUCCESS,
+  });
 };
